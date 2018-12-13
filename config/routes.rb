78de2364c33b5d8bd19ed592517cd7794ea11e10
Benjamin <hoppeconnect@gmail.com>
2018-12-13 09:46:28 -0500

    Completed assignment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :pictures
+  resource :users
+  resource :sessions
+
+  root 'pictures#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181203200127) do
+ActiveRecord::Schema.define(version: 2018_12_12_223952) do
 
   create_table "pictures", force: :cascade do |t|
-    t.string   "artist"
-    t.string   "title"
-    t.string   "url"
+    t.string "artist"
+    t.string "title"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
